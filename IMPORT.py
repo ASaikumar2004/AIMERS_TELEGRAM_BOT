2f82c9b2b69cea3b8ce8e7965b4dd183a3e05e87
ASaikumar2004 <ampolusaikumar2004@gmail.com>
2024-08-20 18:31:58 +0530

    added an import file

---
+++ IMPORT.py
@@ -0,0 +1,5 @@
+!pip install pyTelegramBotAPI
+!pip install openai
+!pip install google-generativeai #For Google Gemini #AIMERS
+!pip install anthropic
+TelegramBOT_TOKEN = '6952759601:AAHCqCyzP1muk0wk2_RJyV08beSh36ZD29w'
